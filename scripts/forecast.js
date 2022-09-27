2f148b280561773170d3ff456e9207c06e34c4ca
georgeleonework <georgeleonework@gmail.com>
2022-09-27 15:58:05 -0400

    added response variable to capture fetch data and the save it as data in json form

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,15 @@
 const key = 'S3o0AV3WZt3YUp9AsAkzoKNLXBwgsYhf';
 
 const getCity = async (city) => {
+
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; // base url of the api endpoint
-    const query = `?apiKey=${key}&q=${city}`
-}
+    const query = `?apikey=${key}&q=${city}`
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    console.log(data)
+
+};
+
+getCity('Miami');

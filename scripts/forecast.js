8826e6e27048f93ce148947d381125a8b16a7faa
georgeleonework <georgeleonework@gmail.com>
2022-09-27 15:53:16 -0400

    added query parameters to getCity function

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,6 @@
 const key = 'S3o0AV3WZt3YUp9AsAkzoKNLXBwgsYhf';
 
-const getCity = async () => {
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search' // base url of the api endpoint
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; // base url of the api endpoint
+    const query = `?apiKey=${key}&q=${city}`
 }

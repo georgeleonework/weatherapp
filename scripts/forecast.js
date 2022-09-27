6f2a5967fc143c9e17758975bab8e1b803346989
georgeleonework <georgeleonework@gmail.com>
2022-09-27 15:50:59 -0400

    added api key and the base url that we want to connect to

---
+++ scripts/forecast.js
@@ -0,0 +1,5 @@
+const key = 'S3o0AV3WZt3YUp9AsAkzoKNLXBwgsYhf';
+
+const getCity = async () => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search' // base url of the api endpoint
+}

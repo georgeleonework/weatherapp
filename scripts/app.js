252187899babc174211445262e7958fc5e3d31a5
georgeleonework <georgeleonework@gmail.com>
2022-09-27 16:38:43 -0400

    added the template to update the innterhtml of the details html to reflect the dynamically updated data

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,18 @@ const updateUI = (data) => {
 
     const cityDets = data.cityDets;
     const weather = data.weather;
+
+    //update details
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.temperature.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    
 };
 
 

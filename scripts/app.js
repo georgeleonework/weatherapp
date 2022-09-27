723afa54c12639c2876c5ef890bfdc75ca8547db
georgeleonework <georgeleonework@gmail.com>
2022-09-27 16:29:42 -0400

    added the vriables to store the query selector for the weatehr card and the weather details

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,19 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
 const updateCity = async (city) => {
-
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
-    
-    return {
-        cityDets,
-        weather
-    };
+    return {cityDets, weather};
 };
 
 cityForm.addEventListener('submit', e => {
 //prevent default action
     e.preventDefault();
-
 //get city value
     const city = cityForm.city.value.trim();
     cityForm.reset();
-
 //update the ui with the new city
     updateCity(city)
         .then(data => console.log(data))

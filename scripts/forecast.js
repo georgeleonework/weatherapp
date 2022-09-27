e1bab7bc383f9a4cc955d41962ac629e3d1d27d4
georgeleonework <georgeleonework@gmail.com>
2022-09-27 16:06:33 -0400

    added the base and query for the getWeather function and then instantiated the response as the resolution of the fetch method

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,5 +1,15 @@
 const key = 'S3o0AV3WZt3YUp9AsAkzoKNLXBwgsYhf';
 
+//get weather forecast information
+
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+}
+
+//get city information
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; // base url of the api endpoint
@@ -8,8 +18,10 @@ const getCity = async (city) => {
     const response = await fetch(base + query);
     const data = await response.json();
 
-    console.log(data)
+    return data[0];
 
 };
 
-getCity('Miami');
+getCity('Miami')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));

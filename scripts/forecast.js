31474544db796e872cec60c3ec16d6c61d8d6725
georgeleonework <georgeleonework@gmail.com>
2022-09-28 09:57:16 -0400

    refactored to object oriented

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,27 +1,30 @@
-const key = 'S3o0AV3WZt3YUp9AsAkzoKNLXBwgsYhf';
+class Forecast{
+    constructor(){
+        this.key = 'S3o0AV3WZt3YUp9AsAkzoKNLXBwgsYhf';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city){
+            const cityDets = await this.getCity(city);
+            const weather = await this.getWeather(cityDets.Key);
+            return {cityDets, weather};
+    }
+    async getCity(city){
+        
+        const query = `?apikey=${this.key}&q=${city}`
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+    
+        return data[0];
+    }
+    async getWeather(id) {
+        
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+    
+        return data[0];
 
-//get weather forecast information
-
-const getWeather = async (id) => {
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-    const query = `${id}?apikey=${key}`;
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
+    }
 }
-
-//get city information
-const getCity = async (city) => {
-
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'; // base url of the api endpoint
-    const query = `?apikey=${key}&q=${city}`
-
-    const response = await fetch(base + query);
-    const data = await response.json();
-
-    return data[0];
-
-};
 
